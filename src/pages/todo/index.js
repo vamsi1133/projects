7e9601cfc1ccc316c85d95a47a654ba4b5f72f44
vamsi krishna raju pericherla <vamsiraju113@gmail.com>
2023-03-13 18:24:14 +0530

    dfdfd

--- src/pages/todo/index.js
+++ src/pages/todo/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./todo.css";
 
 function Todo(props) {
+  // const [tasks, setTasks] = useState([]);
+  // const [task, setTask] = useState("");
+
+  // const addTask = (e) => {
+  //   e.preventDefault();
+  //   setTasks((prev) => [...prev, task]);
+  //   setTask("");
+  // };
+
+  // const removeTask = (e) => {
+  //   const id = e.target.id;
+  //   const temp = [...tasks];
+  //   temp.splice(id, 1);
+  //   setTasks(temp);
+  // };
+
   return (
-    <>
+    <div className="centeritems">
       <h1>ToDO APP</h1>
       <a
         href="https://www.geeksforgeeks.org/javascript-project-on-todo-list/"
@@ -11,8 +28,26 @@ function Todo(props) {
       >
         visit this link for problem statement
       </a>
-      <div>start coding here</div>
-    </>
+      {/* <div className="todo-wrapper">
+        <form>
+          <input
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="enter task"
+            value={task}
+          />
+          <button onClick={addTask}>Add</button>
+        </form>
+        <ul onDoubleClick={removeTask}>
+          {tasks.map((val, index) => {
+            return (
+              <li id={index} className="listitems" key={index}>
+                {val}
+              </li>
+            );
+          })}
+        </ul>
+      </div> */}
+    </div>
   );
 }
 
